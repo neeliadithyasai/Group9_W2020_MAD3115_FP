ed7272b94e8edbcef7963c45834b8f00f42d139b
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-03-15 22:38:40 -0400

    [AN] Created vehicle class and display delegate class

--- Group9_W2020_MAD3115_FP/Vehicle.swift
+++ Group9_W2020_MAD3115_FP/Vehicle.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+enum Fuel
+{
+    case Diesel, Petrol, Electric
+}
+
+protocol Vehicle: DisplayDelegate {
+   
+    var vin: String { get }
+    var vehicle_description: String { get set }
+    var manufacturer_name: String { get set }
+    var is_self_drive: Bool { get  }
+    var driver: String { get set }
+    var is_insured: Bool { get  }
+    var insurance_provider: String { get set }
+    var seats: String { get set }
+    var fuel_type: Fuel { get set }
+    var base_rate: Int { get set }
+    var rate_per_km: Int { get set }
+    
+    func display()
+    
+}
