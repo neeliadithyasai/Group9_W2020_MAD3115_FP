febc173e837fe688e53efd076c983fdbae6c5180
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-03-15 22:50:27 -0400

    [AN] Created car class

---
+++ Group9_W2020_MAD3115_FP/Car.swift
@@ -0,0 +1,9 @@
+//
+//  Car.swift
+//  Group9_W2020_MAD3115_FP
+//
+//  Created by adithyasai neeli on 2020-03-15.
+//  Copyright © 2020 adithyasai neeli. All rights reserved.
+//
+
+import Foundation
