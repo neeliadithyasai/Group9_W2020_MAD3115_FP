5807c5be059318e88196e66030607ef427047a9d
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-03-26 18:46:32 -0400

    [MK] Label added in shared VC

--- Group9_W2020_MAD3115_FP/SharedViewController.swift
+++ Group9_W2020_MAD3115_FP/SharedViewController.swift
@@ -14,9 +14,28 @@ class SharedViewController: UIViewController {
     var owner : Owner?
     var driver : Driver?
     
+    @IBOutlet weak var lblPerson: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let id = customer?.Id, let fn = customer?.fullName, let gn = customer?.gender, let ag = customer?.age, let mn = customer?.mobileNumber,
+            let em = customer?.emailId,
+        let ad = customer?.address,
+            let ct = customer?.city
+    {
+         
+            lblPerson.text = "  Customer ID          :    \(id)\n\n  Customer Name  :    \(fn)\n\n  Customer Gender   :    \(gn)\n\n  Customer Age  :    \(ag)\n\n  Customer MobileNumber  :    \(mn)\n\n  Customer EmailID  :    \(em)\n\n  Customer Address  :    \(ad)\n\n  Customer City  :    \(ct)"
+            lblPerson.numberOfLines = 0
+            lblPerson.font = UIFont.boldSystemFont(ofSize: 12)
+            lblPerson.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            lblPerson.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        }
 
     }
 
