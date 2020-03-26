1047dd4fcd8c085327ca20e0220c17dccb3f3444
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-03-26 13:04:21 -0400

    [MK] Created a new VC from all vehicles display

---
+++ Group9_W2020_MAD3115_FP/AllVehiclesViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AllVehiclesViewController.swift
+//  Group9_W2020_MAD3115_FP
+//
+//  Created by Mohit Kumar on 2020-03-26.
+//  Copyright © 2020 adithyasai neeli. All rights reserved.
+//
+
+import UIKit
+
+class AllVehiclesViewController: UIViewController {
+
+    let segmentedControl : UISegmentedControl = {
+        let sc = UISegmentedControl(items: ["Cars","Motorcycles","Buses"])
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
