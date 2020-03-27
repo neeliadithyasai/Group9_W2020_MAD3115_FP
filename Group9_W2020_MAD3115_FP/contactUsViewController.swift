a4edcc3b93db843708c5b4ac48181436ab9cac17
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-03-27 01:26:55 -0400

    [AN] Added info in about us

--- Group9_W2020_MAD3115_FP/contactUsViewController.swift
+++ Group9_W2020_MAD3115_FP/contactUsViewController.swift
@@ -20,7 +20,7 @@ class contactUsViewController: UIViewController {
     
     func display(){
         if Transition == "aboutUsViewController"{
-            lblContactus.text = "About Us:\n Adithya Sai Neeli- C0771910 \n Mohit Kumar- C0777190\n Manish pawar- C0771915"
+            lblContactus.text = "About Us:\n This is vehicle renting app project created by:\n Adithya Sai Neeli- C0771910 \n Mohit Kumar- C0777190\n Manish pawar- C0771915\n This app contains detailed information of all customers, drivers and owners.\n It also contains the information of all the vehicles and the details of persons who own it.\n User can Add new vehicles , customers, drivers, owners information.\n "
             lblContactus.numberOfLines = 0
         }else{
             if Transition == "contactUsViewController"
