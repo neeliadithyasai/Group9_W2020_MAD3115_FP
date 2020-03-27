1a503e6d5d0181730aef0b101d9586a2a9a23c33
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-03-27 00:35:04 -0400

    [MK] Bus Info getting displayed

--- Group9_W2020_MAD3115_FP/VehicleDisplayViewController.swift
+++ Group9_W2020_MAD3115_FP/VehicleDisplayViewController.swift
@@ -21,19 +21,23 @@ class VehicleDisplayViewController: UIViewController {
         super.viewDidLoad()
         
         if let vin = car?.vin, let vd = car?.vehicle_description, let mn = car?.manufacturer_name, let dr = car?.driver, let sd = car?.is_self_drive, let ip = car?.insurance_provider, let ft = car?.fuel_type, let cc = car?.carColor, let ct = car?.carType, let se = car?.seats, let br = car?.base_rate, let rpk = car?.rate_per_km{
-            if sd == true{
-            lblVehicle.text = "Identification Number : \(vin)\n\nDescription : \(vd)\n\nManufacturer : \(mn)\n\nDriver : \(dr)\n\nSelf Drive Available : Yes\n\nInsurance Provider : \(ip)\n\nFuel Type : \(ft)\n\nColor : \(cc)\n\nType : \(ct)\n\nNumber of Seats : \(se)\n\nBase Rate : \(br)\n\nRate Per Km : \(rpk)"
-        }
-            else {
-                lblVehicle.text = "Identification Number : \(vin)\n\nDescription : \(vd)\n\nManufacturer : \(mn)\n\nDriver : \(dr)\n\nSelf Drive Available : No\n\nInsurance Provider : \(ip)\n\nFuel Type : \(ft)\n\nColor : \(cc)\n\nType : \(ct)\n\nNumber of Seats : \(se)\n\nBase Rate : \(br)\n\nRate Per Km : \(rpk)"
-            }
+        
+            lblVehicle.text = "Identification Number : \(vin)\n\nDescription : \(vd)\n\nManufacturer : \(mn)\n\nDriver : \(dr)\n\nSelf Drive Available : \(sd ? "Yes" : "No")\n\nInsurance Provider : \(ip)\n\nFuel Type : \(ft)\n\nColor : \(cc)\n\nType : \(ct)\n\nNumber of Seats : \(se)\n\nBase Rate : \(br)\n\nRate Per Km : \(rpk)"
+                lblVehicle.numberOfLines = 0
+                lblVehicle.font = UIFont.boldSystemFont(ofSize: 12)
+                lblVehicle.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                lblVehicle.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         }
 
-}
-    
-    
-    
-    
-    
-
+            
+        else if let vin = bus?.vin, let vd = bus?.vehicle_description, let mn = bus?.manufacturer_name, let dr = bus?.driver, let sd = bus?.is_self_drive, let ip = bus?.insurance_provider, let ft = bus?.fuel_type, let asa = bus?.accessibility_service_available, let bt = bus?.busType, let se = bus?.seats, let br = bus?.base_rate, let rpk = bus?.rate_per_km, let wifi = bus?.wifi_available{
+        
+            lblVehicle.text = "Identification Number : \(vin)\n\nDescription : \(vd)\n\nManufacturer : \(mn)\n\nDriver : \(dr)\n\nSelf Drive Available : Yes\n\nInsurance Provider : \(ip)\n\nFuel Type : \(ft)\n\nAccessibility Service Available : \(asa ? "Yes" : "No")\n\nType : \(bt)\n\nNumber of Seats : \(se)\n\nBase Rate : \(br)\n\nRate Per Km : \(rpk)\n\nWifi Available : \(wifi ?  "Yes" : "No")"
+                lblVehicle.numberOfLines = 0
+                lblVehicle.font = UIFont.boldSystemFont(ofSize: 12)
+                lblVehicle.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                lblVehicle.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        }
+        
+    }
 }
